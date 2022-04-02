ee6c83b3e28a1ae91acb8396505763dd1fca61d4
SendMuj <itachi7779@gmail.com>
2022-04-01 19:09:43 -0500

    just messed with GUI a little, and had idea for the GUI file going forward, added as comment

--- basicMenuGUI.py
+++ basicMenuGUI.py
@@ -4,12 +4,12 @@ import PySimpleGUI as sg
 
 #Main Menu GUI
 def MainMenu():
-    layout = [ [sg.Text("""Gestures:          Options:
-                         \nIndex Up          Mouse Control
-                    Index and Ring Up Audio Control
-                    First Three Up    App Launcher
-                    Four Up           WAH!
-                    Index+Thumb+Pinky Shutdown""")]
+    layout = [ [sg.Text("""Gestures:                Options: 
+                         \nIndex Up                  Mouse Control
+                         \nIndex and Ring Up    Audio Control
+                         \nFirst Three Up          App Launcher
+                         \nFour Up                   WAH!
+                         \nIndex+Thumb+Pinky Shutdown""")]
         
     ]
     
@@ -18,6 +18,18 @@ def MainMenu():
 
 
 #Mouse Control GUI
+def MouseMenu():
+    layout = [ [sg.Text("""Gestures:             Options: 
+                         \nIndex Up               Move Mouse
+                         \nIndex and Ring Up Left Click
+                         \nFirst Three Up       Double Left Click
+                         \nFour Up                Open/Close UI
+                         \nThumb+Pinky   Shutdown""")]
+        
+    ]
+    
+    menu = sg.Window("Move Mouse", layout, location =(0,0), no_titlebar=True)
+    menu.read(timeout=0)
 
 #Audio Control GUI
 
@@ -30,3 +42,10 @@ def MainMenu():
 #Quitting UI
 
 #Closing Existing UI
+
+
+
+
+#make menu class, have default menu active, with ways of closing it. then have functions that replace it when going into other menus/states
+#call the class in the main menu function, then call the functions that replace the main menu in subsequent menu changes
+#this way can make a function that closes the menu, regardless of which is up
